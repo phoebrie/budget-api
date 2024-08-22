f4db5aca07fc595706c5eefe4d022e6915f07f49
phoebrie <phoebe.lickiss10@gmail.com>
2024-08-22 10:45:47 +0100

    post to add envelopes

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require("express");
+// const fetch = (url, init) =>
+//   import("node-fetch").then((module) => module.default(url, init));
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 const port = 3000;
 
+const envelopes = [];
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json(envelopes);
+});
+
+app.post("/envelopes", (req, res) => {
+  const total_budget = req.body;
+  console.log(total_budget);
+  envelopes.push(total_budget);
+  res.json({ envelopes });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// http://localhost:3000/ <- use this to see webapp.
