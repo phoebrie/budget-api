60011d60335214b3d4f4bfd986cdcdbc804029d5
phoebrie <phoebe.lickiss10@gmail.com>
2024-08-22 11:37:39 +0100

    adding fashion envelope

--- app.js
+++ app.js
@@ -7,16 +7,28 @@ app.use(bodyParser.json());
 const port = 3000;
 
 const envelopes = [];
+const fashion_envelopes = [];
 
 app.get("/", (req, res) => {
   res.json(envelopes);
 });
 
+app.get("/envelopes/:envelope", (req, res) => {
+  res.json(fashion_envelopes);
+});
+
 app.post("/envelopes", (req, res) => {
-  const total_budget = req.body;
-  console.log(total_budget);
-  envelopes.push(total_budget);
+  const envelope = req.body;
+  console.log(envelope);
+  envelopes.push(envelope);
   res.json({ envelopes });
+});
+
+app.post("/envelopes/fashion", (req, res) => {
+  const fashion_envelope = req.body;
+  console.log(fashion_envelope);
+  fashion_envelopes.push(fashion_envelope);
+  res.json({ fashion_envelopes });
 });
 
 app.listen(port, () => {
