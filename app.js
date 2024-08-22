9bf748d241da29fbc00bfac10eef4269c0f938c1
phoebrie <phoebe.lickiss10@gmail.com>
2024-08-22 13:09:13 +0100

    post request with param

--- app.js
+++ app.js
@@ -7,28 +7,32 @@ app.use(bodyParser.json());
 const port = 3000;
 
 const envelopes = [];
-const fashion_envelopes = [];
+
+const example_envelope = {
+  id: "1234",
+  budget: 5000,
+  description: "hello world",
+};
 
 app.get("/", (req, res) => {
   res.json(envelopes);
 });
 
-app.get("/envelopes/:envelope", (req, res) => {
-  res.json(fashion_envelopes);
+app.get("/envelopes/:id", (req, res) => {
+  const envelope_id = req.params["id"];
+  const envelope = envelopes.find((e) => e.id === envelope_id);
+  res.json(envelope);
 });
 
 app.post("/envelopes", (req, res) => {
   const envelope = req.body;
-  console.log(envelope);
+  const existing_envelope = envelopes.find((e) => e.id === envelope.id);
+  if (existing_envelope != null) {
+    res.status(500).send();
+  }
+
   envelopes.push(envelope);
   res.json({ envelopes });
-});
-
-app.post("/envelopes/fashion", (req, res) => {
-  const fashion_envelope = req.body;
-  console.log(fashion_envelope);
-  fashion_envelopes.push(fashion_envelope);
-  res.json({ fashion_envelopes });
 });
 
 app.listen(port, () => {
